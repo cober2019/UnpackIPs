e023e45c1fc2e51f5d832b57d0adb57f6f403191
cober2019 <53579603+cober2019@users.noreply.github.com>
2020-12-13 20:29:45 -0800

    Add files via upload

--- UnPackIps.py
+++ UnPackIps.py
@@ -5,6 +5,7 @@ import subprocess as sp
 import logging
 import time
 
+subnet = None
 logging.basicConfig(filename='ping.log', level=logging.INFO)
 
 
@@ -15,10 +16,8 @@ def _diagnose(ip, status) -> logging:
 
         try:
             if i.split()[0] == "Reply":
-                print(
-                    f"{ip} up | Latency: {i.split()[4].split('=')[1]} Time: {time.strftime('%a, %d %b %Y %H:%M:%S +0000')}")
-                logging.info(
-                    f"\n          {ip} up | Latency: {i.split()[4].split('=')[1]} Time: {time.strftime('%a, %d %b %Y %H:%M:%S +0000')}")
+                print(f"{ip} up | Latency: {i.split()[4].split('=')[1]} Time: {time.strftime('%a, %d %b %Y %H:%M:%S +0000')}")
+                logging.info(f"\n          {ip} up | Latency: {i.split()[4].split('=')[1]} Time: {time.strftime('%a, %d %b %Y %H:%M:%S +0000')}")
             elif i.split()[0] == "Ping" or "Packets" or "Approximate" or "Minimum":
                 pass
 
@@ -45,17 +44,27 @@ def _is_ip(ip):
 
 def _get_subnets(user_input):
     # Remove space to avoind ip address errors
+    global subnet
+
     ips = user_input.replace(" ", "")
 
     """Private method for getting subnet and fourth octet info."""
     ip_details = []
     for subnet in ips.split('|'):
-        octets = subnet.split('.')
-        subnet = '.'.join(octets[0:-1])
-        ranges = octets[3]
-        if len(octets) != 4:
-            ValueError("There was more than 4 octets found: " + ips)
-        ip_details.append({"subnet": subnet, "ranges": ranges})
+        if '/' in subnet:
+            [ip_details.append({"subnet": str(subnet), "ranges": str(i)}) for i in ipaddress.ip_network(subnet)]
+        else:
+            octets = subnet.split('.')
+            subnet = '.'.join(octets[0:-1])
+
+            try:
+                ranges = octets[3]
+            except IndexError:
+                pass
+
+            if len(octets) != 4:
+                ValueError("There was more than 4 octets found: " + ips)
+            ip_details.append({"subnet": subnet, "ranges": ranges})
 
     return ip_details
 
@@ -80,22 +89,29 @@ def get_ips(user_input: str = None) -> list:
     ip_details = _get_subnets(user_input)
     expanded_ip = []
     check_overlap = {}
+
     for item in ip_details:
-        fourth_octets = _mixrange(item['ranges'])
-        for fourth_octet in fourth_octets:
-            ip = item['subnet'] + '.' + str(fourth_octet)
-
-            # Verify if there is an IP address, skip ip is function return False
-            valid_ip = _is_ip(ip)
-            if valid_ip is False:
-                continue
-
-            # Check if this IP converted to an integer has been found already, using the dictionary
-            # to determine if this IP was seen already.
-            if check_overlap.get(int(ipaddress.IPv4Address(ip))):
-                raise ValueError("Overlapping IP: " + ip)
-            check_overlap[int(ipaddress.IPv4Address(ip))] = True
-            expanded_ip.append(ip)
+        # Check for CIDR
+        if '/' in item['subnet']:
+            # exclode .0
+            if item['ranges'][-1] != '0':
+                expanded_ip.append(item['ranges'])
+        else:
+            fourth_octets = _mixrange(item['ranges'])
+            for fourth_octet in fourth_octets:
+                ip = item['subnet'] + '.' + str(fourth_octet)
+    
+                # Verify if there is an IP address, skip ip is function return False
+                valid_ip = _is_ip(ip)
+                if valid_ip is False:
+                    continue
+    
+                # Check if this IP converted to an integer has been found already, using the dictionary
+                # to determine if this IP was seen already.
+                if check_overlap.get(int(ipaddress.IPv4Address(ip))):
+                    raise ValueError("Overlapping IP: " + ip)
+                check_overlap[int(ipaddress.IPv4Address(ip))] = True
+                expanded_ip.append(ip)
 
     return expanded_ip
 
@@ -107,4 +123,3 @@ def ping(user_input: str = None):
         ping_ips = sp.getstatusoutput("ping " + i)
         _diagnose(i, ping_ips)
         print("\n")
-
